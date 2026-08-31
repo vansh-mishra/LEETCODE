5e8900b4d72575294c4c21b19aa9ba872da86452
VANSH MISHRA <vanshmishra11110@gmail.com>
2026-08-31 09:02:11 +0530

    Time: 0 ms (100%), Space: 48.1 MB (91.85%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -2,22 +2,21 @@ class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length-1;
-        int ans = -1;
-        while(right>=left) {
-            int mid = (right+left)/2;
-            if(nums[mid] == target) {
-                ans=mid;
-                break;
-            } else if(nums[mid]<target){
-                left = mid+1;
-            } else {
-                right = mid -1;
+        int mid;
+        while(left<=right) {
+            mid = (left+right)/2;
+            if(nums[mid]<target) {
+                left = mid + 1;
+            }
+            else if(nums[mid]>target) {
+                right = mid - 1;
+            }
+            else {
+                return mid;
             }
         }
-        if (ans==-1){
-            return ans;
-        } else {
-            return ans;
-        }
+        return -1;
+
+
     }
 }
