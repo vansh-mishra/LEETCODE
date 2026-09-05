9585b72c424a1b064cd4b373689a6baf875d2f49
VANSH MISHRA <vanshmishra11110@gmail.com>
2026-09-05 12:57:30 +0530

    Time: 0 ms (100%), Space: 47.5 MB (32.72%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,8 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int mis = 0;
-        for(int i = 1;i<=nums.length;i++) {
-            mis ^= i;
-        }
-        for (int num: nums) {
-            mis ^= num;
+        int mis = nums.length;
+        for(int i = 0;i<nums.length;i++) {
+            mis ^= i ^nums[i];
         }
         return mis;
     }
