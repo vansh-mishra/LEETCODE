76b7f37c099fa0612603169e5accfa9d81b0858a
VANSH MISHRA <vanshmishra11110@gmail.com>
2026-09-08 12:38:05 +0530

    Time: 778 ms (12.98%), Space: 260.4 MB (5.46%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,29 +1,27 @@
-import java.util.Arrays;
-
 class Solution {
     public int countPrimes(int n) {
-        if (n <= 2) return 0; 
-
-        boolean[] arr = new boolean[n];
-        Arrays.fill(arr, true);
-
-        arr[0] = false;
-        arr[1] = false;
-
-        for (int i = 2; i * i < n; i++) {
-            if (arr[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    arr[j] = false;
+        if(n<=2){
+            return 0;
+        }
+        return PrimeSeive(n);
+    }
+    public static int PrimeSeive(int n ){
+        int[] prime = new int[n];
+        prime[0]= 1; // yeh prime number nhi h 
+        prime[1] = 1; //yeh prime number nhi h 
+        for(int i = 2;i*i<=prime.length;i++){
+            if(prime[i]==0){
+                for(int j = 2;i*j<prime.length;j++){
+                    prime[i*j]=1;
                 }
             }
         }
-
-        int count = 0;
-        for (int i = 2; i < n; i++) {
-            if (arr[i]) {
-                count++;
+        int c = 0;
+        for(int i = 2;i<prime.length;i++){
+            if(prime[i]==0){
+                c++;
             }
         }
-        return count;
+        return c ;
     }
 }
