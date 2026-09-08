73ef414498301ebe42178a0d9c455232804d333a
VANSH MISHRA <vanshmishra11110@gmail.com>
2026-09-08 12:36:54 +0530

    Time: 703 ms (22.91%), Space: 79.8 MB (16.45%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0; 
+
+        boolean[] arr = new boolean[n];
+        Arrays.fill(arr, true);
+
+        arr[0] = false;
+        arr[1] = false;
+
+        for (int i = 2; i * i < n; i++) {
+            if (arr[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    arr[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (arr[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
