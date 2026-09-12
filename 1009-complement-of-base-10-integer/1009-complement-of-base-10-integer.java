dfab4c7ee092957a30d8404def9612ee566dc47e
VANSH MISHRA <vanshmishra11110@gmail.com>
2026-09-12 21:12:55 +0530

    Time: 0 ms (100%), Space: 42.1 MB (53.93%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,8 @@
+// Added using AI
+class Solution {
+    public int bitwiseComplement(int n) {
+        if (n == 0) return 1;
+        int mask = (1 << (32 - Integer.numberOfLeadingZeros(n))) - 1;
+        return n ^ mask;
+    }
+}
